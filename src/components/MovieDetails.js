97caeb65af55029272cfd0f621042a4320e29431
amal-gafsi <amal.gafsi1@esprit.tn>
2022-01-04 14:58:16 +0100

    update

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -1,10 +1,21 @@
 import React from "react";
 import ReactPlayer from "react-player";
+import { Button } from "react-bootstrap";
+import { useHistory } from "react-router-dom";
 
-export default function movieDetails({ match, movies }) {
+export default function MovieDetails({ match, movies }) {
+  let history = useHistory();
+
   let movie = movies.find((el) => el.title === match.params.name);
   return (
     <div style={{ marginTop: "3%" }}>
+      <Button
+        variant="primary"
+        onClick={() => history.goBack()}
+        style={{ marginRight: "80%" }}
+      >
+        Go Back
+      </Button>
       <h1 style={{ color: "red", textDecoration: "underline" }}>
         {movie.title}
       </h1>
